2ec7f36797b7002d3d78c3c66b6eed454b369b49
Lucas Campos <lucasgcampos.contato@gmail.com>
2016-06-11 12:12:32 -0300

    CollapsingToolbarLayout

--- Carros/app/src/main/java/com/lgcampos/carros/activity/CarroActivity.java
+++ Carros/app/src/main/java/com/lgcampos/carros/activity/CarroActivity.java
@@ -1,10 +1,12 @@
 package com.lgcampos.carros.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.lgcampos.carros.R;
 import com.lgcampos.carros.domain.Carro;
 import com.lgcampos.carros.fragments.CarroFragment;
+import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
 
@@ -19,9 +21,13 @@ public class CarroActivity extends BaseActivity {
         getSupportActionBar().setTitle(carro.nome);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        ImageView appBarImg = (ImageView) findViewById(R.id.app_bar_img);
+        Picasso.with(getContext()).load(carro.urlFoto).into(appBarImg);
+
         if (savedInstanceState == null) {
             CarroFragment fragment = new CarroFragment();
             fragment.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.carro_fragment, fragment).commit();
         }
 
     }
