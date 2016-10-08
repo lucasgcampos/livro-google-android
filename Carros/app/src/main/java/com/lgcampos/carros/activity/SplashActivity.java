d56fdbb7c2cae48e0d8cf8a117556e34f8ef7854
Lucas Campos <lucasgcampos.contato@gmail.com>
2016-10-08 11:42:04 -0300

    22.5

--- Carros/app/src/main/java/com/lgcampos/carros/activity/SplashActivity.java
+++ Carros/app/src/main/java/com/lgcampos/carros/activity/SplashActivity.java
@@ -22,7 +22,9 @@ public class SplashActivity extends BaseActivity {
 
         String permisions[] = new String[] {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
         };
 
         boolean hasPermission = PermissionUtils.validate(this, 0, permisions);
